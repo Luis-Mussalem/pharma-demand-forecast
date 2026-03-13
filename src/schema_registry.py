01d63451d395acf35b6c347b5e69fc0f941a7e27
Luis Mussalem <luismussalem@gmail.com>
2026-03-13 16:50:35 -0300

    create training schema registry

---
+++ src/schema_registry.py
@@ -0,0 +1,24 @@
+TRAINING_SCHEMA_V1 = {
+    "columns": [
+        "Store",
+        "DayOfWeek",
+        "Date",
+        "Sales",
+        "Customers",
+        "Open",
+        "Promo",
+        "StateHoliday",
+        "SchoolHoliday",
+    ],
+    "dtypes": {
+        "Store": "int64",
+        "DayOfWeek": "int64",
+        "Date": "datetime64[ns]",
+        "Sales": "int64",
+        "Customers": "int64",
+        "Open": "int64",
+        "Promo": "int64",
+        "StateHoliday": "category",
+        "SchoolHoliday": "int64",
+    },
+}
